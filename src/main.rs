3ffc809e0af11b030b4db906763e247124cd266b
Hansamu <tintin30052002@gmail.com>
2025-06-11 07:48:34 -0400

    The type builtin: builtins

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+static BUILTIN_COMMANDS: [&str; 3] = ["type", "echo", "exit"];
+
 fn main() {
 	// Wait for user input
     loop {
@@ -13,11 +15,22 @@ fn main() {
 
 		// Validate input
 		match input.trim() {
+			cmd if cmd.starts_with("type") => {
+				let test_cmd = cmd.trim_start_matches("type ");
+				if BUILTIN_COMMANDS.contains(&test_cmd) {
+					println!("{} is a shell builtin", test_cmd);
+				} else {
+					println!("{}: not found", test_cmd);
+				}
+			}
+			
 			cmd if cmd.starts_with("echo ") => {
 				let message = cmd.trim_start_matches("echo ");
 				println!("{}", message);
 			},
+
 			"exit 0" => std::process::exit(0),
+
 			cmd => println!("{}: command not found", cmd),
 		}
     }
