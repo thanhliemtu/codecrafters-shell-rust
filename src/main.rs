5d8fdac191807706341d5538973f40cacf7d7dd3
Hansamu <tintin30052002@gmail.com>
2025-06-11 07:29:01 -0400

    The exit builtin

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,19 @@ use std::io::{self, Write};
 fn main() {
 	// Wait for user input
     loop {
+		// Prompt the user for input and read a line
+		// Flush stdout to ensure the prompt is displayed immediately
 		print!("$ ");
 		io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim());
+
+		// Validate input
+		if input == "exit 0\n" || input == "exit 0\r\n" {
+			std::process::exit(0)
+		} 
+		else {
+			println!("{}: command not found", input.trim());
+		}
     }
 }
