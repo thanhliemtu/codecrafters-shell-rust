e7cf74e5f7f19fdbb35f0035291603b417fd2d51
Hansamu <tintin30052002@gmail.com>
2025-06-23 13:04:13 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -336,7 +336,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 			// Handle external commands, i.e., commands not in the built-in list
 			_ => {
-				if let Some(cmd_path) = path_commands.get(cmd) {
+				if let Some(_) = path_commands.get(cmd) {
 					let mut child = Command::new(cmd);
 
 					child.args(argv)                     
